4ec7d86ec8f4238006e9f09cab5318501c5b85fd
Jordan Sandjo <jordansandjo@gmail.com>
2025-07-05 19:48:15 +0200

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from "./footer/footer";
+import { Main } from "./main/main";
+import { Header } from "./header/header";
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Footer, Main, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
   protected title = 'profiles';
 }
---
+++ src/app/main/main.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [],
+  templateUrl: './main.html',
+  styleUrl: './main.css'
+})
+export class Main {
+
+}
